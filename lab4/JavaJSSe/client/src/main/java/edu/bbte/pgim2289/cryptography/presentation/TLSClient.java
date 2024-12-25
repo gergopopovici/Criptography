4dfc8882a9258d7073a97b0a9a04bd5e67499622
gergopopovici <popovicigergo0330@gmail.com>
2024-12-25 17:39:38 +0200

    Added TLSServer redid the TLSClient logic to handle fake connections.

--- lab4/JavaJSSe/client/src/main/java/edu/bbte/pgim2289/cryptography/presentation/TLSClient.java
+++ lab4/JavaJSSe/client/src/main/java/edu/bbte/pgim2289/cryptography/presentation/TLSClient.java
@@ -1,61 +1,66 @@
 package edu.bbte.pgim2289.cryptography.presentation;
 
 import javax.net.ssl.*;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URL;
-import java.security.KeyManagementException;
-import java.security.NoSuchAlgorithmException;
+import java.security.KeyStore;
 import java.security.cert.Certificate;
 import java.security.cert.X509Certificate;
-import java.security.SecureRandom;
 
 public class TLSClient {
     public static void main(String[] args) {
         String httpsURL = "https://bnr.ro/Home.aspx";
         String output = "bnr_response.html";
+        String trustStorePath = "C:/egyetem/3harmadik ev/elso felev/Criptography/lab4/JavaJSSe/truststore.jks";
+        String trustStorePassword = "password";
+
+        String expectedRealCN = "*.bnr.ro";
+
         try {
-            TrustManager[] trustAllCerts = new TrustManager[]{
-                    new X509TrustManager() {
-                        public X509Certificate[] getAcceptedIssuers() {
-                            return null;
-                        }
+            KeyStore trustStore = KeyStore.getInstance(KeyStore.getDefaultType());
+            trustStore.load(new FileInputStream(trustStorePath), trustStorePassword.toCharArray());
 
-                        public void checkClientTrusted(X509Certificate[] certs, String authType) {
-                        }
-
-                        public void checkServerTrusted(X509Certificate[] certs, String authType) {
-                        }
-                    }
-            };
+            TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
+            tmf.init(trustStore);
 
             SSLContext sslContext = SSLContext.getInstance("TLS");
-            sslContext.init(null, trustAllCerts, new SecureRandom());
+            sslContext.init(null, tmf.getTrustManagers(), new java.security.SecureRandom());
             HttpsURLConnection.setDefaultSSLSocketFactory(sslContext.getSocketFactory());
 
             URL url = new URL(httpsURL);
             HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
             connection.connect();
 
+            Certificate[] serverCertificates = connection.getServerCertificates();
+            boolean isRealServer = false;
+
             System.out.println("Certificate Information:");
-            Certificate[] certs = connection.getServerCertificates();
-            for (Certificate cert : certs) {
+            for (Certificate cert : serverCertificates) {
                 if (cert instanceof X509Certificate) {
                     X509Certificate x509Cert = (X509Certificate) cert;
-                    System.out.println("Subject: " + x509Cert.getSubjectDN());
-                    System.out.println("Issuer: " + x509Cert.getIssuerDN());
+
+                    String subjectDN = x509Cert.getSubjectX500Principal().getName();
+                    String subjectCN = extractCN(subjectDN);
+
+                    System.out.println("Subject: " + subjectDN);
+                    System.out.println("Issuer: " + x509Cert.getIssuerX500Principal().getName());
                     System.out.println("Serial Number: " + x509Cert.getSerialNumber());
                     System.out.println("Valid From: " + x509Cert.getNotBefore());
                     System.out.println("Valid Until: " + x509Cert.getNotAfter());
-                    System.out.println("Public Key: " + x509Cert.getPublicKey());
-                    System.out.println("Version: " + x509Cert.getVersion());
+
+                    if (expectedRealCN.equals(subjectCN)) {
+                        isRealServer = true;
+                        break;
+                    }
                 }
             }
 
+            if (!isRealServer) {
+                throw new SecurityException("Certificate validation failed. This may not be the authentic Romanian National Bank server!");
+            }
+
             BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-            BufferedWriter writer = new BufferedWriter(new java.io.FileWriter(output));
+            BufferedWriter writer = new BufferedWriter(new FileWriter(output));
             String line;
             while ((line = reader.readLine()) != null) {
                 writer.write(line);
@@ -63,8 +68,26 @@ public class TLSClient {
             }
             reader.close();
             writer.close();
-        } catch (KeyManagementException | NoSuchAlgorithmException | IOException e) {
+            System.out.println("HTML response saved to " + output);
+
+        } catch (SecurityException e) {
             System.out.println("Error: " + e.getMessage());
+            System.out.println("Warning: Potential man-in-the-middle attack detected!");
+        } catch (javax.net.ssl.SSLHandshakeException e) {
+            System.out.println("Error: SSL handshake failed. Possible certificate mismatch!");
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            e.printStackTrace();
         }
+    }
+
+    private static String extractCN(String distinguishedName) {
+        for (String part : distinguishedName.split(",")) {
+            String trimmed = part.trim();
+            if (trimmed.startsWith("CN=")) {
+                return trimmed.substring(3);
+            }
+        }
+        return null;
     }
 }
--- lab4/JavaJSSe/server/src/main/java/edu/bbte/pgim2289/cryptography/backend/TLSServer.java
+++ lab4/JavaJSSe/server/src/main/java/edu/bbte/pgim2289/cryptography/backend/TLSServer.java
@@ -1,7 +1,66 @@
 package edu.bbte.pgim2289.cryptography.backend;
 
+import javax.net.ssl.*;
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.security.*;
+import java.security.cert.CertificateException;
+
 public class TLSServer {
     public static void main(String[] args) {
-        System.out.println("TLS Server");
+        int port = 443;
+        String htmlFilePath = "C:/egyetem/3harmadik ev/elso felev/Criptography/lab4/JavaJSSe/client/bnr_response.html";
+
+        try {
+            KeyStore keyStore = KeyStore.getInstance("JKS");
+            try (FileInputStream keyStoreFile = new FileInputStream("C:/egyetem/3harmadik ev/elso felev/Criptography/lab4/JavaJSSe/fake_bnr.jks")) {
+                keyStore.load(keyStoreFile, "password".toCharArray());
+            }
+
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+            kmf.init(keyStore, "password".toCharArray());
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(kmf.getKeyManagers(), null, null);
+            SSLServerSocketFactory ssf = sslContext.getServerSocketFactory();
+            try (SSLServerSocket serverSocket = (SSLServerSocket) ssf.createServerSocket()) {
+                serverSocket.bind(new InetSocketAddress(port));
+                System.out.println("Fake BNR server is running on port " + port);
+
+                while (true) {
+                    try (SSLSocket clientSocket = (SSLSocket) serverSocket.accept()) {
+                        System.out.println("Client connected: " + clientSocket.getInetAddress());
+                        handleClient(clientSocket, htmlFilePath);
+                    } catch (SocketException se) {
+                        System.err.println("Client connection dropped: " + se.getMessage());
+                    } catch (IOException e) {
+                        System.err.println("Error processing client request: " + e.getMessage());
+                    }
+                }
+            }
+        } catch (KeyStoreException | IOException | NoSuchAlgorithmException | CertificateException |
+                 UnrecoverableKeyException |
+                 KeyManagementException e) {
+            System.err.println("Error starting server: " + e.getMessage());
+        }
+    }
+
+    private static void handleClient(SSLSocket clientSocket, String htmlFilePath) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+             BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream()))) {
+            String line;
+            while ((line = reader.readLine()) != null && !line.isEmpty()) {
+                System.out.println(line);
+            }
+            String htmlContent = Files.readString(Path.of(htmlFilePath));
+            writer.write("HTTP/1.1 200 OK\r\n");
+            writer.write("Content-Type: text/html\r\n");
+            writer.write("Content-Length: " + htmlContent.length() + "\r\n");
+            writer.write("\r\n");
+            writer.write(htmlContent);
+            writer.flush();
+        }
     }
 }
